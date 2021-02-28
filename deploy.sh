7e5bd0937960d6f27a6e1b86a68a79b7692ac344
lixichen <lixichen@yy.com>
2021-03-01 00:41:28 +0800

    feat: 测试vuepress部署

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,21 @@
 
 # 确保脚本抛出遇到的错误
 set -e
-
+# 跳到docs文件夹
+cd packages/docs&demo
+# 执行初始化
+npm i
 # 生成静态文件
 npm run docs:build
 
 # 进入生成的文件夹
-cd packages/docs&demo/docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:warpcgd/killblanks.git master:gh-pages
+git push -f https://${access_token}@github.com/warpcgd/killblanks.git master:gh-pages
 
 cd -
